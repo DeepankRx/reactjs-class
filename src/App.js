5a881d1ce94bb7240e68480cd5f9ef73218b0cac
unknown <deepankpushpad10@gmail.com>
2024-04-21 22:58:06 +0530

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import axios from 'axios';
-import './App.css';
-import { useEffect, useState } from 'react';
-const url = 'http://localhost:3001';
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
+const url = "http://localhost:3001";
 
 function App() {
   // [stateName,function] = useState(initialValue)
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState({});
   // const increaseValueBy1 = (e) => {
   //   setValue(value + 1);
   // };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      await axios.post(`${url}/add`, {
+      const res = await axios.post(`${url}/add`, {
         title,
       });
+      console.log(res.data.data);
+      setTodos([...todos, res.data.data]);
+      setTitle("");
     } catch (error) {
       console.log(error);
     }
   };
-  console.log('a');
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(`${url}/update/${selectedTodo._id}`, {
+        title,
+      });
+
+      // .map , .filter always returns an array
+
+      setTodos(
+        todos.map((item) => {
+          if (item._id === selectedTodo._id) {
+            return {
+              ...item,
+              title,
+            };
+          }
+          return item;
+        })
+      );
+      setIsEdit(false);
+      setTitle("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log("a");
   const getAllTodos = async () => {
     try {
       const res = await axios.get(`${url}/all`);
@@ -33,18 +62,47 @@ function App() {
     }
   };
   useEffect(() => {
-    console.log('inside useEffect');
+    console.log("inside useEffect");
     getAllTodos();
   }, []);
+  const handleDelete = async (itemId) => {
+    try {
+      await axios.delete(`${url}/delete/${itemId}`);
+      // setTodos(res.data.data);
+      setTodos(todos.filter((item) => item._id !== itemId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEdit = (item) => {
+    setTitle(item.title);
+    setSelectedTodo(item);
+    setIsEdit(true);
+  };
 
   return (
     <div className="App">
-      <input
-        type="text"
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-      />
+      <div className="input-group input-group-sm mb-3">
+        <span className="input-group-text" id="inputGroup-sizing-sm">
+          Title
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          type="button"
+          class="btn btn-primary"
+          onClick={isEdit ? handleUpdate : handleSubmit}
+        >
+          {isEdit ? "Update" : "Submit"}
+        </button>
+      </div>
       {/* <input
         type="email"
         onChange={(e) => {
@@ -57,12 +115,31 @@ function App() {
           setPhone(e.target.value);
         }}
       /> */}
-      {todos.map((item) => (
-        <p>{item.title}</p>
-      ))}
-      <button type="text" onClick={handleSubmit}>
-        Submit
-      </button>
+      <div className="d-flex flex-wrap gap-2">
+        {todos.map((item) => (
+          <div className="card w-25">
+            <div className="card-body">
+              <h5 className="card-title">{item.title}</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <button
+                className="btn btn-info mx-3"
+                onClick={() => handleEdit(item)}
+              >
+                Edit
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleDelete(item._id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
